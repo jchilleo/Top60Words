04c829ac3f17f3de74982cc7cca6f4d4c3010e32
jchilleo <jchilleo2011@gmail.com>
2015-08-30 23:13:49 -0400

    Clean up
    cleaned up the code, removed unnecessary code and finished java docs.

--- src/Top60.java
+++ src/Top60.java
@@ -1,10 +1,7 @@
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 import static java.lang.System.out;
 
-import java.util.ArrayList;
 
 public class Top60 {
 	ArrayList<Word> wordList = new ArrayList<Word>();
@@ -12,11 +9,12 @@ public class Top60 {
 	boolean flag = false;
 
 	public static void main(String[] args) {
-		
 		Top60 top60 = new Top60();
 		top60.readTextFile();
+		top60.findTop60Words();
+		top60.printTop60Words();
 	}
-		
+	
 	private void readTextFile(){
 		java.io.File file = new java.io.File("Artamenes.txt");
 		try{
@@ -24,15 +22,12 @@ public class Top60 {
 			while (input.hasNext()){
 			String buffer = input.nextLine();
 			buffer = buffer.toLowerCase();
-			bufferParse(buffer);
-				 
+			bufferParse(buffer);	 
 			}
 			input.close();
 		}
-		catch (Exception e){
-			System.err.format("File does not exist\n");
-		}
-		findTop60Words();
+		catch (Exception e){System.err.format("File does not exist\n");}
+		
 	}
 	/**
 	 * Parse string buffers to remove the following punctuation and characters:
@@ -40,7 +35,7 @@ public class Top60 {
 	 * @param buffer - String of words read from a text file that needs to be parsed.
 	 */
 	private void bufferParse(String buffer){
-	String delimiters = "[ ,.;:()?!\\[\\]]+";
+	String delimiters = "[ ,.;:()?!\\[\\]\t]+";
 	String[] parsedBufferArray = buffer.split(delimiters);
 	extractBufferArray(parsedBufferArray);
 	}
@@ -52,89 +47,42 @@ public class Top60 {
 	void extractBufferArray(String[] parsedBufferArray){
 
 		int length = parsedBufferArray.length;
-		boolean contains = false;
 		for(int i = 0; i < length ; i ++){
-			//if (flag == false) {out.println("1"); flag = true;}
 			if(parsedBufferArray[i] != null || parsedBufferArray[i] != ""){
 				Word newWord = new Word(parsedBufferArray[i]);
-				//out.println(newWord.getName() + " || " + parsedBufferArray[i]);
 			if(wordList.isEmpty()){
-				//out.println(newWord.getName());
-				wordList.add(newWord);
-				}
+				wordList.add(newWord);}
 			else{
-			//Iterator<Word> itr = wordList.iterator();
-			//while(itr.hasNext()){
-				//Word searchWord = itr.next();
-				/*
-				for(Word searchWord: wordList){
-					if(searchWord.getName() !=null && searchWord.getName().contains(newWord.getName()));{
-						searchWord.updateOccurrence();
-						//out.println(searchWord.getName());
-						contains = true;}
-					//if(flag == true){out.println("2");flag = false;}
-					}
-				if(contains == false){
-					wordList.add(newWord);
-					contains = false;
-				}
-				*/
 				int index = wordList.indexOf(newWord);
-				if(index == -1)
-				{
-					wordList.add(newWord);
-				}
+				if(index == -1){	wordList.add(newWord);}
 				else
-				{
-					wordList.get(index).updateOccurrence();
-				}
+				{wordList.get(index).updateOccurrence();}
 				}}}}
 	
 	/**
 	 * Takes an array list of type <Word> and finds the top 60 word occurrences and moves them to a new array list.
 	 */
 	private void findTop60Words(){
-		/*
-		int count = 0;
-		for(int i = 0; i <= 60 ; i++){
-			for(Word topWordCount: wordList){
-				if(topWordCount.getOccurrence() > count){
-					out.println(topWordCount.getName());
-					count = topWordCount.getOccurrence();
-					//if(flag == false){out.println("3");flag = true;}
-				}
-			}
-			for(Word topWord: wordList){
-				if(topWord.getOccurrence() == count){
-					top60Words.add(topWord);
-					wordList.remove(topWord);
-					break;
-				}
-			}
-		count = 0;}
-		*/
-		
 		java.util.Collections.sort(wordList);
-		for(int i = 0; i<= 60; i++){
+		for(int i = 0; i<= 200; i++){
 			top60Words.add(wordList.get(i));
 		}
-		printTop60Words();
 	}
-	
+	/**
+	 * Prints out the top 60 most used words from the text file.
+	 */
 	private void printTop60Words(){
-		
 		out.println("Word" + '\t'+ '\t' + "Occurences" + '\t' + "Percentage of Occurrence");
 		out.println("--------------------------------------------------------");
 		for(Word printWord: top60Words){
+			if(printWord.getName().length() > 7){
+				out.print(printWord.getName() + "\t" + printWord.getOccurrence() + "\t\t");
+				out.printf("%.2f%%\n",printWord.getPercentage());
+			}
+			else{
 			out.print(printWord.getName() + "\t\t" + printWord.getOccurrence() + "\t\t");
 			out.printf("%.2f%%\n",printWord.getPercentage());
-		
+			}
 		}
 	}
-	
-	
-	
-	
-	
-
 }
